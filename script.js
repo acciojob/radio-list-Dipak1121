4ad83a683b28524a59ecbc03a716b8034556da53
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 17:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let radio = document.querySelectorAll("input");
+radio[1].disabled = true;
+radio[2].checked = true;
+let lebels = document.querySelectorAll("label");
+for ( let i = 0; i < lebels.length; i++ ){
+	if(i%2 == 1){
+		lebels[i].style.color = "red";
+	}
+}
